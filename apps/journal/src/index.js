0de4d9a5a8de45790e9c0170618e93c0e5458deb
JCamiloo <jcamilo.osorio15@gmail.com>
2022-02-16 10:25:17 -0500

    init project

---
+++ apps/journal/src/index.js
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom';
+import { JournalApp } from './JournalApp';
+import './styles.scss';
+
+ReactDOM.render(
+  <JournalApp />,
+  document.getElementById('root')
+);
