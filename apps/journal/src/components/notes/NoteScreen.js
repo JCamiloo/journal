d629defdf13cefa6fb855546b73091a5d860937b
JCamiloo <jcamilo.osorio15@gmail.com>
2022-02-18 14:38:04 -0500

    NoteScreen styles

--- apps/journal/src/components/notes/NoteScreen.js
+++ apps/journal/src/components/notes/NoteScreen.js
@@ -4,6 +4,28 @@ export const NoteScreen = () => {
   return (
     <div className="notes__main-content">
       <NotesAppBar/>
+
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+
+        <textarea
+          placeholder="What happened today"
+          className="notes__textarea"
+        >
+        </textarea>
+
+        <div className="notes__image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb2R05MFgxxUFBxU1tu82poXTIpVb_XEgFwg&usqp=CAU"
+            alt="sea"
+          />
+        </div>
+      </div>
     </div>
   )
 }
